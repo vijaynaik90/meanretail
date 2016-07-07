9ea1fd02746f84cf68a68a14dbdd9baa56a01ba0
vijaynaik90 <naik.vi@husky.neu.edu>
2016-07-07 19:42:25 -0400

    env change

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ wagner.invoke(require('./auth'), { app: app });
 app.use('/api/v1', require('./api')(wagner));
 
 // Serve up static HTML pages from the file system.
-// For instance, '/6-examples/hello-http.html' in
-// the browser will show the '../6-examples/hello-http.html'
-// file.
 app.use(express.static('./public', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 /*
 app.set('views', __dirname + '/../');
@@ -24,13 +21,12 @@ app.get('/', function(req, res){
   res.sendfile('index.html', {root: app.settings.views});
 });
 */
-/*var port = process.ENV.PORT || 4000
-app.listen(port);
-console.log('Listening on port'+ port);
-*/
 
-var server = app.listen(process.env.PORT || 4000, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+var port = process.env.PORT || 4000
+app.listen(port,function(){
+console.log('Listening on port'+ port);	
 });
+
+
+
+
