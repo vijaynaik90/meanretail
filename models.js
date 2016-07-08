8d268bbc022211807daa0d832be34d544e0c078a
vijaynaik90 <naik.vi@husky.neu.edu>
2016-07-07 22:49:05 -0400

    mongodb_uri

--- models.js
+++ models.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var _ = require('underscore');
 
 module.exports = function(wagner) {
-  mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost:27017/test');
+  mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/test');
 
   wagner.factory('db', function(){
     return mongoose;
